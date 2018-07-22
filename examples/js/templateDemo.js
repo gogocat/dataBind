13a7b73cb57bc527a897a73350606f2be3d2aedb
adam <adamchow2326@yahoo.com.au>
2018-07-22 23:52:19 +1000

    updated template demo

--- examples/js/templateDemo.js
+++ examples/js/templateDemo.js
@@ -5,17 +5,6 @@
         heading: 'Test template binding',
         description: 'This is intro and it is looking good!',
         isDisplay: true,
-        onHover: {
-            in: function(e, $el) {
-                let text = $el.text();
-                $el.data('prevText', text);
-                $el.text('**' + text + '**');
-            },
-            out: function(e, $el) {
-                let text = $el.data('prevText') || '';
-                $el.text(text);
-            },
-        },
         updateView(opt) {
             this.APP.render(opt);
         },
