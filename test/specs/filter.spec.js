38e9682bc9cf6e60c9d6ee3fdd5e8a50d8bb2156
Chow, Adam <wchow@ebay.com>
2018-10-10 16:32:19 +1100

    update filter unit test

--- test/specs/filter.spec.js
+++ test/specs/filter.spec.js
@@ -3,41 +3,39 @@ describe('Given [data-jq-comp="filter-component"] inited', () => {
 
     jasmine.getFixtures().fixturesPath = 'test';
 
-    beforeEach(function() {
+    beforeEach(function(done) {
         loadFixtures('./fixtures/filters.html');
 
         namespace.viewModel = {
             renderIntro: false,
             heading: 'Test data-if-binding',
             description: 'This is intro text',
+            discountRate: 0.8,
+            gstRate: 1.1,
             story: {
                 title: 'Hansel and Gretel',
                 description:
                     '"Hansel and Gretel" (also known as Hansel and Grettel, Hansel and Grethel, or Little Brother and Little Sister) is a well-known fairy tale of German origin.',
                 link: 'https://www.google.com.au/search?q=Hansel+and+Gretel',
+                price: 100,
             },
-            viewModelPropFn: function($data) {
-                return typeof $data.viewModelPropFn === 'function';
+            toDiscount: function(value) {
+                return Number(value) * this.discountRate;
             },
-            undefinedViewModelPropFn: function($data) {
-                return;
-            },
-            setStroylinkAttr: function($data) {
-                return {
-                    href: this.story.link,
-                    title: this.story.title,
-                    target: '_blank',
-                    rel: 'noopener noreferrer',
-                };
+            addGst: function(value) {
+                return Number(value) * this.gstRate;
             },
             updateView: function(opt) {
                 return this.APP.render(opt);
             },
         };
 
-        namespace.filterComponent = dataBind.init($('[data-jq-comp="filter-component"]'), namespace.viewModel);
+        // jasmine spies
+        spyOn(namespace.viewModel.toDiscount, 'toDiscount');
+        spyOn(namespace.viewModel.addGst, 'addGst');
 
-        namespace.filterComponent.render();
+        namespace.filterComponent = dataBind.init($('[data-jq-comp="filter-component"]'), namespace.viewModel);
+        namespace.filterComponent.render().then(() => done());
     });
 
     afterEach(() => {
@@ -49,16 +47,13 @@ describe('Given [data-jq-comp="filter-component"] inited', () => {
         }
     });
 
-    it('Then [data-jq-comp="filter-component"] should render story with once filter and not intro', (done) => {
-        setTimeout(() => {
-            let $intro = document.getElementById('intro');
-            let $story = document.getElementById('story');
+    it('Then [data-jq-comp="filter-component"] should render story with once filter and not intro', () => {
+        let $intro = document.getElementById('intro');
+        let $story = document.getElementById('story');
 
-            expect($intro).toBe(null);
-            expect($story).not.toBe(null);
-            expect($story.firstElementChild).not.toBe(null);
-            done();
-        }, 200);
+        expect($intro).toBe(null);
+        expect($story).not.toBe(null);
+        expect($story.firstElementChild).not.toBe(null);
     });
 
     it('Should render intro but not story section after update viewModel', (done) => {
@@ -70,5 +65,17 @@ describe('Given [data-jq-comp="filter-component"] inited', () => {
             expect($story).toBe(null);
             done();
         });
+    });
+
+    it('Should render story and stroyPrice pass through filters | toDiscount | addGst', () => {
+        let $story = document.getElementById('story');
+        let stroyPrice = document.getElementById('stroyPrice').textContent;
+        let discountedPrice = namespace.viewModel.toDiscount(namespace.viewModel.story.price);
+        let finalPrice = namespace.viewModel.addGst(discountedPrice);
+
+        expect($story).not.toBe(null);
+        expect(namespace.viewModel.toDiscount).toHaveBeenCalledWith(namespace.viewModel.story.price);
+        expect(namespace.viewModel.addGst).toHaveBeenCalledWith(discountedPrice);
+        expect(stroyPrice).toBe(finalPrice);
     });
 });
