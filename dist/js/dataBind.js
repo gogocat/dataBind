d4c8a77f18ed1d40a2a52a8af5b76bd31772b3be
adam <adamchow2326@yahoo.com.au>
2021-09-22 22:19:27 +1000

    upgraded packages

--- dist/js/dataBind.js
+++ dist/js/dataBind.js
@@ -11,7 +11,7 @@
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
     typeof define === 'function' && define.amd ? define(factory) :
     (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.dataBind = factory());
-}(this, (function () { 'use strict';
+})(this, (function () { 'use strict';
 
     const bindingAttrs$1 = {
       comp: 'data-bind-comp',
@@ -2669,4 +2669,4 @@
 
     return index;
 
-})));
+}));
